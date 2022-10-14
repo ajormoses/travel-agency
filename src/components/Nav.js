9bb454fe7ab9a5f1c9076d5ab2c237bcd6c178dc
ajormoses <ajormoses@gmail.com>
2022-10-14 16:09:24 +0100

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,14 @@
-const Nav = () => {
+const Nav = (props) => {
   return (
-    <nav className="nav">
+    <nav
+      className="nav"
+      data-aos="fade-right"
+      data-aos-offset="300"
+      data-aos-easing="ease-in-sine"
+      data-aos-duration="500"
+    >
       <ul className="nav-menu">
-        <a href="#article-one">
+        <a href="#article-one" onClick={props.toggle}>
           <li>Discover</li>
         </a>
         <a href="#article-two">
